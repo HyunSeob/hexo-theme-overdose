deb080e68f119865a4934dd2d68572cb47c03776
HyunSeob <leehs1017@naver.com>
2016-08-23 01:29:37 +0900

    Implement head block

---
+++ scripts/canonical.js
@@ -0,0 +1,13 @@
+/**
+* Canonical Helper
+* @description Generate canonical tag.
+* @example
+*     canonical(config, page);
+*/
+
+hexo.extend.helper.register('canonical', function (config, page) {
+  var base_url = config.url;
+  if (config.url.charAt(config.url.length - 1) !== '/') base_url += '/';
+
+  return '<link rel="canonical" href="' + base_url + page.canonical_path.replace('index.html', '') + '"/>';
+});
