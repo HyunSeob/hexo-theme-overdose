9c8f0d5c09fba3aace6caad6610ff491b6d06a86
HyunSeob <leehs1017@naver.com>
2016-09-19 00:58:43 +0900

    optimize dynamic menu for mobile

--- source/js/dynamicMenu.js
+++ source/js/dynamicMenu.js
@@ -16,8 +16,12 @@
     var menu = document.getElementById('menu');
     var article = document.getElementById('article');
     var articleBottom = article ? article.offsetTop + article.scrollHeight : null;
+    var throttled = false;
 
     window.addEventListener('scroll', function() {
+
+      if (throttled) return;
+      throttled = true;
 
       currPos = window.scrollY || window.pageYOffset || document.body.scrolTop;
 
@@ -33,6 +37,10 @@
 
       prevPos = currPos;
 
+      setTimeout(function() {
+        throttled = false;
+      }, 100);
+      
     });
   };
 })(window, document);
