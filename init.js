47c5a05aa36d9c2771c0ef909363c77ff1703b80
HyunSeob <sytte1017@gmail.com>
2017-10-06 23:16:39 +0900

    Delete unneeded scripts

--- init.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var path = require('path');
-var deleteFolderRecursive = require('./util').deleteFolderRecursive;
-var input = path.resolve(__dirname, '_config.yml.example');
-var output = path.resolve(__dirname, '_config.yml');
-
-fs.createReadStream(input).pipe(fs.createWriteStream(output));
-deleteFolderRecursive(path.resolve(__dirname, '.git'));
-fs.unlink(path.resolve(__dirname, '.gitignore'));
